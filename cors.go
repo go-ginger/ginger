96e38b0b211c0efa8aea82340c31bc9a1650ff9f
saeed <saeid.kamyabi71@gmail.com>
2019-08-20 16:28:18 +0430

    fix cors, fix headers value

--- cors.go
+++ cors.go
@@ -6,7 +6,7 @@ func CORS(c *gin.Context) {
 	if config.CorsEnabled {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", config.CorsAllowOrigins)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", config.CorsAllowCredentials)
-		c.Writer.Header().Set("Access-Control-Allow-Headers", config.CorsAllowCredentials)
+		c.Writer.Header().Set("Access-Control-Allow-Headers", config.CorsAllowHeaders)
 		c.Writer.Header().Set("Access-Control-Allow-Methods", config.CorsAllowMethods)
 	}
 	c.Next()
